a00051441584b40aa688b5ae82350053e1f94f5a
dhanwantanishka <dhanwantanishka4169@gmail.com>
2025-06-15 11:09:07 +0530

    [Uncommented lines]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -31,7 +31,7 @@ public class Main {
     // Uncomment this block to pass the first stage
     // 
     if (fileContents.length() > 0) {
-    //   throw new RuntimeException("Scanner not implemented");
+      throw new RuntimeException("Scanner not implemented");
     } else {
      System.out.println("EOF  null"); // Placeholder, replace this line when implementing the scanner
     // }
