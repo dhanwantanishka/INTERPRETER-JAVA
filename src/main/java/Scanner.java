c4a8d373ef6308c335efdfa3c4545a343c05f4cb
dhanwantanishka <dhanwantanishka4169@gmail.com>
2025-06-18 22:56:13 +0530

    [ Support for scanning number literals. ]

--- src/main/java/Scanner.java
+++ src/main/java/Scanner.java
@@ -1,153 +1,127 @@
+import java.util.Map;
+
 public class Scanner {
-  private int current = 0;
-  private String file;
-  private int line = 1;
-  static private boolean hadError = false;
+  private final Map<String, String> tokens =
+      Map.of("(", "LEFT_PAREN", ")", "RIGHT_PAREN", "{", "LEFT_BRACE", "}",
+             "RIGHT_BRACE", "*", "STAR", ".", "DOT", ",", "COMMA", "+", "PLUS",
+             "-", "MINUS", ";", "SEMICOLON");
 
-  Scanner(String fileContents) { this.file = fileContents; }
+  public boolean scan(String fileContents) {
+    boolean isValidToken = true;
+    int LnNBR = 1;
+    int start, end;
+    String val;
+    String numberPattern = "[0-9\\\\.]";
 
-  private char advance() { return file.charAt(current++); }
+    if (fileContents.length() > 0) {
+      for (int i = 0; i < fileContents.length(); i++) {
+        char c = fileContents.charAt(i);
+        // System.err.printf("i is %d c is %c%n", i, c);
+        if (tokens.containsKey(String.valueOf(c))) {
+          System.out.println(tokens.get(String.valueOf(c)) + " " + c + " null");
+        } else if (c == '=') {
+          if (((i + 1) < fileContents.length()) &&
+              (fileContents.charAt(i + 1) == '=')) {
+            i = i + 1;
+            System.out.println("EQUAL_EQUAL == null");
+          } else {
+            System.out.println("EQUAL = null");
+          }
+        } else if (c == '!') {
+          if (((i + 1) < fileContents.length()) &&
+              (fileContents.charAt(i + 1) == '=')) {
+            i = i + 1;
+            System.out.println("BANG_EQUAL != null");
+          } else {
+            System.out.println("BANG ! null");
+          }
+        } else if (c == '<') {
+          if (((i + 1) < fileContents.length()) &&
+              (fileContents.charAt(i + 1) == '=')) {
+            i = i + 1;
+            System.out.println("LESS_EQUAL <= null");
+          } else {
+            System.out.println("LESS < null");
+          }
+        } else if (c == '>') {
+          if (((i + 1) < fileContents.length()) &&
+              (fileContents.charAt(i + 1) == '=')) {
+            i = i + 1;
+            System.out.println("GREATER_EQUAL >= null");
+          } else {
+            System.out.println("GREATER > null");
+          }
+        } else if (c == '/') {
+          if (((i + 1) < fileContents.length()) &&
+              (fileContents.charAt(i + 1) == '/')) {
+            i = i + 1;
+            while ((i < fileContents.length()) &&
+                   (fileContents.charAt(i) != '\n')) {
+              i = i + 1;
+            }
+            LnNBR = LnNBR + 1;
+          } else {
+            System.out.println("SLASH / null");
+          }
+        } else if (c == '\n' || c == ' ' || c == '\t') {
+          if (c == '\n') {
+            LnNBR = LnNBR + 1;
+          }
+        } else if (c == '"') {
+          //  string tokens;
+          i = i + 1;
+          start = i;
+          while ((i < fileContents.length()) &&
+                 (fileContents.charAt(i) != '"')) {
+            // System.err.println("c is " + c);
+            i = i + 1;
+          }
+          System.err.println("start " + start + "] end " + i);
+          if (i >= fileContents.length()) {
+            System.err.println("[line " + LnNBR +
+                               "] Error: Unterminated string.");
+            isValidToken = false;
 
-  private void scanToken() {
-    char c = advance();
-    switch (c) {
-    case '(':
-      System.out.println("LEFT_PAREN ( null");
-      break;
-    case ')':
-      System.out.println("RIGHT_PAREN ) null");
-      break;
-    case '}':
-      System.out.println("RIGHT_BRACE } null");
-      break;
-    case '{':
-      System.out.println("LEFT_BRACE { null");
-      break;
-    case '*':
-      System.out.println("STAR * null");
-      break;
-    case '+':
-      System.out.println("PLUS + null");
-      break;
-    case '.':
-      System.out.println("DOT . null");
-      break;
-    case ',':
-      System.out.println("COMMA , null");
-      break;
-    case '-':
-      System.out.println("MINUS - null");
-      break;
-    case '/':
-      if (match('/')) {
-        while (peek() != '\n' && !isAtEnd())
-          advance();
-      } else {
-        System.out.println("SLASH / null");
+          } else {
+            end = i;
+
+            val = fileContents.substring(start, end);
+            System.out.println("STRING " +
+                               fileContents.substring(start - 1, end + 1) +
+                               " " + val);
+          }
+          start = 0;
+          end = 0;
+
+        } else if (String.valueOf(c).matches(numberPattern)) {
+          // number tokens;
+          start = i;
+          while (
+              (i < fileContents.length()) &&
+              (String.valueOf(fileContents.charAt(i)).matches(numberPattern))) {
+            i = i + 1;
+            System.err.println("c is " + fileContents.charAt(i - 1) + " i is " +
+                               i);
+          }
+          end = i;
+          val = fileContents.substring(start, end);
+          System.err.println("val is " + val);
+          double num = Double.parseDouble(val);
+          System.out.println("NUMBER " + val + " " + num);
+          i = i - 1;
+
+        }
+
+        else {
+          System.err.println("[line " + LnNBR +
+                             "] Error: Unexpected character: " + c);
+          isValidToken = false;
+        }
       }
-      break;
-    case ';':
-      System.out.println("SEMICOLON ; null");
-      break;
-    case '\n':
-      line++;
-      break;
-    case '=':
-      if (match('=')) {
-        System.out.println("EQUAL_EQUAL == null");
-        break;
-      }
-
-      System.out.println("EQUAL = null");
-      break;
-    case '!':
-      if (match('=')) {
-        System.out.println("BANG_EQUAL != null");
-        break;
-      }
-
-      System.out.println("BANG ! null");
-      break;
-    case '>':
-      if (match('=')) {
-        System.out.println("GREATER_EQUAL >= null");
-        break;
-      }
-
-      System.out.println("GREATER > null");
-      break;
-    case '<':
-      if (match('=')) {
-        System.out.println("LESS_EQUAL <= null");
-        break;
-      }
-
-      System.out.println("LESS < null");
-      break;
-    case '"':
-      string();
-      break;
-    case ' ':
-    case '\r':
-    case '\t':
-      // Ignore whitespace.
-      break;
-    default:
-      System.err.printf("[line %d] Error: Unexpected character: %c\n", line, c);
-      hadError = true;
-      break;
-    }
-  }
-
-  private void string() {
-    int start = current - 1;
-    while (peek() != '"' && !isAtEnd()) {
-      if (peek() == '\n') line++;
-      advance();
-    }
-
-    if (isAtEnd()) {
-      System.err.printf("[line %d] Error: Unterminated string.\n", line);
-      hadError = true;
-      return;
-    }
-
-    // The closing ".
-    advance();
-
-    // Trim the surrounding quotes.
-    String value = file.substring(start + 1, current - 1);
-    System.out.println("STRING " + file.substring(start, current) + " " + value);
-  }
-
-  private char peek() {
-    if (isAtEnd())
-      return '\0';
-    return file.charAt(current);
-  }
-
-  private boolean isAtEnd() { return current >= file.length(); }
-
-  private boolean match(char expected) {
-    if (isAtEnd())
-      return false;
-    if (file.charAt(current) != expected)
-      return false;
-
-    current++;
-    return true;
-  }
-
-  public void scanTokens() {
-    while (!isAtEnd()) {
-      scanToken();
-    }
-
-    if (hadError) {
       System.out.println("EOF  null");
-      System.exit(65);
+    } else {
+      System.out.println("EOF  null");
     }
-
-    System.out.println("EOF  null");
+    return isValidToken;
   }
-} 
+}
