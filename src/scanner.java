8b550504b22b8852a4f6a49e40b2f4c3477f8e27
dhanwantanishka <dhanwantanishka4169@gmail.com>
2025-06-18 22:10:00 +0530

    [ Support for scanning string literals. ]

--- src/scanner.java
+++ src/scanner.java
@@ -1,136 +1,104 @@
+import java.util.Map;
 
-    import java.util.ArrayList;
-import java.util.List;
+public class Scanner {
+  private final Map<String, String> tokens =
+      Map.of("(", "LEFT_PAREN", ")", "RIGHT_PAREN", "{", "LEFT_BRACE", "}",
+             "RIGHT_BRACE", "*", "STAR", ".", "DOT", ",", "COMMA", "+", "PLUS",
+             "-", "MINUS", ";", "SEMICOLON");
 
-public class scanner {
-  private final String source;
-  private final List<Token> tokens = new ArrayList<Token>();
-  private int start = 0;
-  private int current = 0;
-  private int line = 1;
+  public boolean scan(String fileContents) {
+    boolean isValidToken = true;
+    int LnNBR = 1;
+    int start, end;
+    String val;
 
-  Scanner(String source) { this.source = source; }
+    if (fileContents.length() > 0) {
+      for (int i = 0; i < fileContents.length(); i++) {
+        char c = fileContents.charAt(i);
+        // System.err.printf("i is %d c is %c%n", i, c);
+        if (tokens.containsKey(String.valueOf(c))) {
+          System.out.println(tokens.get(String.valueOf(c)) + " " + c + " null");
+        } else if (c == '=') {
+          if (((i + 1) < fileContents.length()) &&
+              (fileContents.charAt(i + 1) == '=')) {
+            i = i + 1;
+            System.out.println("EQUAL_EQUAL == null");
+          } else {
+            System.out.println("EQUAL = null");
+          }
+        } else if (c == '!') {
+          if (((i + 1) < fileContents.length()) &&
+              (fileContents.charAt(i + 1) == '=')) {
+            i = i + 1;
+            System.out.println("BANG_EQUAL != null");
+          } else {
+            System.out.println("BANG ! null");
+          }
+        } else if (c == '<') {
+          if (((i + 1) < fileContents.length()) &&
+              (fileContents.charAt(i + 1) == '=')) {
+            i = i + 1;
+            System.out.println("LESS_EQUAL <= null");
+          } else {
+            System.out.println("LESS < null");
+          }
+        } else if (c == '>') {
+          if (((i + 1) < fileContents.length()) &&
+              (fileContents.charAt(i + 1) == '=')) {
+            i = i + 1;
+            System.out.println("GREATER_EQUAL >= null");
+          } else {
+            System.out.println("GREATER > null");
+          }
+        } else if (c == '/') {
+          if (((i + 1) < fileContents.length()) &&
+              (fileContents.charAt(i + 1) == '/')) {
+            i = i + 1;
+            while ((i < fileContents.length()) &&
+                   (fileContents.charAt(i) != '\n')) {
+              i = i + 1;
+            }
+            LnNBR = LnNBR + 1;
+          } else {
+            System.out.println("SLASH / null");
+          }
+        } else if (c == '\n' || c == ' ' || c == '\t') {
+          if (c == '\n') {
+            LnNBR = LnNBR + 1;
+          }
+        } else if (c == '"') {
+          i = i + 1;
+          start = i;
+          while ((i < fileContents.length()) &&
+                 (fileContents.charAt(i) != '"')) {
+            System.err.println("c is " + c);
+            i = i + 1;
+          }
+          System.err.println("start " + start + "] end " + i);
+          if (i >= fileContents.length()) {
+            System.err.println("[line " + LnNBR +
+                               "] Error: Unterminated string.");
+            isValidToken = false;
 
-  List<Token> scanTokens() {
-    while (!isAtEnd()) {
-      start = current;
-      scanToken();
-    }
+          } else {
+            end = i;
 
-    tokens.add(new Token(TokenType.EOF, "", null, line));
-    return tokens;
-  }
+            val = fileContents.substring(start, end);
+            System.out.println("STRING " +
+                               fileContents.substring(start - 1, end + 1) +
+                               " " + val);
+          }
 
-  private boolean isAtEnd() { return current >= source.length(); }
-
-  private void scanToken() {
-    char c = advance();
-    switch (c) {
-    case '(':
-      addToken(TokenType.LEFT_PAREN);
-      break;
-    case ')':
-      addToken(TokenType.RIGHT_PAREN);
-      break;
-    case '{':
-      addToken(TokenType.LEFT_BRACE);
-      break;
-    case '}':
-      addToken(TokenType.RIGHT_BRACE);
-      break;
-    case ',':
-      addToken(TokenType.COMMA);
-      break;
-    case '*':
-      addToken(TokenType.STAR);
-      break;
-    case '+':
-      addToken(TokenType.PLUS);
-      break;
-    case '-':
-      addToken(TokenType.MINUS);
-      break;
-    case '.':
-      addToken(TokenType.DOT);
-      break;
-    case ';':
-      addToken(TokenType.SEMICOLON);
-      break;
-    case '=':
-      addToken(match('=') ? TokenType.EQUAL_EQUAL : TokenType.EQUAL);
-      break;
-    case '!':
-      addToken(match('=') ? TokenType.BANG_EQUAL : TokenType.BANG);
-      break;
-    case '<':
-      addToken(match('=') ? TokenType.LESS_EQUAL : TokenType.LESS);
-      break;
-    case '>':
-      addToken(match('=') ? TokenType.GREATER_EQUAL : TokenType.GREATER);
-      break;
-    case '/':
-      if (match('/')) {
-        while (peek() != '\n' && !isAtEnd())
-          advance();
-      } else {
-        addToken(TokenType.SLASH);
+        } else {
+          System.err.println("[line " + LnNBR +
+                             "] Error: Unexpected character: " + c);
+          isValidToken = false;
+        }
       }
-      break;
-    case ' ':
-    case '\r':
-    case '\t':
-      break;
-    case '\n':
-      line++;
-      break;
-    case '"':
-      string();
-      break;
-    default:
-      Main.error(line, "Unexpected character", c);
+      System.out.println("EOF  null");
+    } else {
+      System.out.println("EOF  null");
     }
-  }
-
-  private char advance() { return source.charAt(current++); }
-
-  private void addToken(TokenType type) { addToken(type, null); }
-
-  private void addToken(TokenType type, Object literal) {
-    String text = source.substring(start, current);
-    tokens.add(new Token(type, text, literal, line));
-  }
-
-  private boolean match(char expected) {
-    if (isAtEnd())
-      return false;
-    if (source.charAt(current) != expected)
-      return false;
-    current++;
-    return true;
-  }
-
-  private char peek() {
-    if (isAtEnd())
-      return '\0';
-    return source.charAt(current);
-  }
-
-  private void string() {
-    while (peek() != '"' && !isAtEnd()) {
-      if (peek() == '\n')
-        line++;
-      advance();
-    }
-
-    if (isAtEnd()) {
-      Main.error(line, "Unterminated string");
-      return;
-    }
-
-    advance();
-
-    String value = source.substring(start + 1, current - 1);
-    addToken(TokenType.STRING, value);
+    return isValidToken;
   }
 }
-
