821a4a5a38907b88c33883a79082cb0aee1c69b9
dhanwantanishka <dhanwantanishka4169@gmail.com>
2025-06-18 22:13:37 +0530

    [ Support for scanning string literals. ]

---
+++ src/main/java/Scanner.java
@@ -0,0 +1,129 @@
+public class Scanner {
+  private int current = 0;
+  private String file;
+  private int line = 1;
+  static private boolean hadError = false;
+
+  Scanner(String fileContents) { this.file = fileContents; }
+
+  private char advance() { return file.charAt(current++); }
+
+  private void scanToken() {
+    char c = advance();
+    switch (c) {
+    case '(':
+      System.out.println("LEFT_PAREN ( null");
+      break;
+    case ')':
+      System.out.println("RIGHT_PAREN ) null");
+      break;
+    case '}':
+      System.out.println("RIGHT_BRACE } null");
+      break;
+    case '{':
+      System.out.println("LEFT_BRACE { null");
+      break;
+    case '*':
+      System.out.println("STAR * null");
+      break;
+    case '+':
+      System.out.println("PLUS + null");
+      break;
+    case '.':
+      System.out.println("DOT . null");
+      break;
+    case ',':
+      System.out.println("COMMA , null");
+      break;
+    case '-':
+      System.out.println("MINUS - null");
+      break;
+    case '/':
+      if (match('/')) {
+        while (peek() != '\n' && !isAtEnd())
+          advance();
+      } else {
+        System.out.println("SLASH / null");
+      }
+      break;
+    case ';':
+      System.out.println("SEMICOLON ; null");
+      break;
+    case '\n':
+      line++;
+      break;
+    case '=':
+      if (match('=')) {
+        System.out.println("EQUAL_EQUAL == null");
+        break;
+      }
+
+      System.out.println("EQUAL = null");
+      break;
+    case '!':
+      if (match('=')) {
+        System.out.println("BANG_EQUAL != null");
+        break;
+      }
+
+      System.out.println("BANG ! null");
+      break;
+    case '>':
+      if (match('=')) {
+        System.out.println("GREATER_EQUAL >= null");
+        break;
+      }
+
+      System.out.println("GREATER > null");
+      break;
+    case '<':
+      if (match('=')) {
+        System.out.println("LESS_EQUAL <= null");
+        break;
+      }
+
+      System.out.println("LESS < null");
+      break;
+    case ' ':
+    case '\r':
+    case '\t':
+      // Ignore whitespace.
+      break;
+    default:
+      System.err.printf("[line %d] Error: Unexpected character: %c\n", line, c);
+      hadError = true;
+      break;
+    }
+  }
+
+  private char peek() {
+    if (isAtEnd())
+      return '\0';
+    return file.charAt(current);
+  }
+
+  private boolean isAtEnd() { return current >= file.length(); }
+
+  private boolean match(char expected) {
+    if (isAtEnd())
+      return false;
+    if (file.charAt(current) != expected)
+      return false;
+
+    current++;
+    return true;
+  }
+
+  public void scanTokens() {
+    while (!isAtEnd()) {
+      scanToken();
+    }
+
+    if (hadError) {
+      System.out.println("EOF  null");
+      System.exit(65);
+    }
+
+    System.out.println("EOF  null");
+  }
+} 
