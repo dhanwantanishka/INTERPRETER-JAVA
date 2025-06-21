611da154a4c6d08719325adf973660797b270a1b
dhanwantanishka <dhanwantanishka4169@gmail.com>
2025-06-21 21:03:20 +0530

    [implementation for support for the parse command and handle parsing true , false , and nil literals ]

--- src/main/java/Parse.java
+++ src/main/java/Parse.java
@@ -4,8 +4,16 @@ public class Parse {
   public void parse() {
     List<Token> tokens = GlobalStorage.getTokens();
     for (Token token : tokens) {
-      if ("TRUE".equals(token.type)) {
-        System.out.println("true");
+      switch (token.type) {
+        case "TRUE":
+          System.out.println("true");
+          break;
+        case "FALSE":
+          System.out.println("false");
+          break;
+        case "NIL":
+          System.out.println("nil");
+          break;
       }
     }
   }
